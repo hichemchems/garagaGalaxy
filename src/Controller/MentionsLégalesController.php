389a485780a245b309fb2accdbf3142a35aacfc0
hichemchems <154635773+hichemgit-hub@users.noreply.github.com>
2024-04-04 17:27:40 +0200

    create Controller MentionsLegales

--- src/Controller/MentionsLégalesController.php
+++ src/Controller/MentionsLégalesController.php
@@ -6,14 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class MentionsLégalesController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/mentions/l/gales', name: 'app_mentions_l_gales')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+        return $this->render('mentions_légales/index.html.twig', [
+            'controller_name' => 'MentionsLégalesController',
         ]);
     }
-
 }
